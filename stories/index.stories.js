c36dbb39ce2b8f8f4be64050274d724cb8f59d4c
PaulPu <paul.pu.cyberbiz@gmail.com>
2023-02-15 16:58:18 +0800

    :fire: remove unnecessary stories first

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -2,23 +2,23 @@ import React from 'react';
 
 import { MemoryRouter } from 'react-router';
 
-import FlexStory from '@stories/components/FlexStory';
-import PitayaFail from '@src/components/pitayas/PitayaFail';
-import BreadcrumbStorybook from '@stories/breadcrumbStorybook';
-import FormModalStorybook from '@stories/formStorybook';
-import ButtonStorybook from '@stories/buttonStorybook';
-import CheckboxStorybook from '@stories/checkboxStorybook';
-import TextareaStorybook from '@stories/textareaStorybook';
-import SwitchStorybook from '@stories/switchStorybook';
-import SelectStorybook from '@stories/selectStorybook';
-import TabsStorybook from '@stories/tabsStorybook';
-import FileInputStorybook from '@stories/fileInputStorybook';
-import PaginationStorybook from '@stories/paginationStorybook';
-import CheckOptionsStorybook from '@stories/checkOptionsStorybook';
-import ConfirmStorybook from '@stories/ConfirmStorybook';
-import PitayaTooltipStory from '@stories/components/PitayaTooltipStory';
-import PitayaActionSelectorStory from '@stories/components/pitayas/PitayaActionSelectorStorybook';
-import NestableStorybook from '@stories/nestableStorybook';
+// import FlexStory from '@stories/components/FlexStory';
+// import PitayaFail from '@src/components/pitayas/PitayaFail';
+// import BreadcrumbStorybook from '@stories/breadcrumbStorybook';
+// import FormModalStorybook from '@stories/formStorybook';
+// import ButtonStorybook from '@stories/buttonStorybook';
+// import CheckboxStorybook from '@stories/checkboxStorybook';
+// import TextareaStorybook from '@stories/textareaStorybook';
+// import SwitchStorybook from '@stories/switchStorybook';
+// import SelectStorybook from '@stories/selectStorybook';
+// import TabsStorybook from '@stories/tabsStorybook';
+// import FileInputStorybook from '@stories/fileInputStorybook';
+// import PaginationStorybook from '@stories/paginationStorybook';
+// import CheckOptionsStorybook from '@stories/checkOptionsStorybook';
+// import ConfirmStorybook from '@stories/ConfirmStorybook';
+// import PitayaTooltipStory from '@stories/components/PitayaTooltipStory';
+// import PitayaActionSelectorStory from '@stories/components/pitayas/PitayaActionSelectorStorybook';
+// import NestableStorybook from '@stories/nestableStorybook';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -31,63 +31,63 @@ import {
   faPlus,
 } from '@fortawesome/free-solid-svg-icons';
 
-import i18n from '@src/locale/i18n';
+// import i18n from '@src/locale/i18n';
 
 library.add(faEye, faEyeSlash, faEdit, faTrashAlt, faPlus);
 
 export default {
   title: 'Pitaya',
 
-  decorators: [
-    story => <MemoryRouter initialEntries={['/']}>{story()}</MemoryRouter>,
-  ],
+  // decorators: [
+  //   story => <MemoryRouter initialEntries={['/']}>{story()}</MemoryRouter>,
+  // ],
 };
 
-export const PitayaFlex = () => <FlexStory />;
-export const PitayaBreadcrumb = () => <BreadcrumbStorybook />;
-export const PitayaButton = () => <ButtonStorybook />;
-export const PitayaCheckbox = () => <CheckboxStorybook />;
-export const PitayaTextarea = () => <TextareaStorybook />;
-export const PitayaSelect = () => <SelectStorybook />;
-export const _PitayaFail = () => <PitayaFail connectStatus />;
-export const PitayaFormModal = () => <FormModalStorybook />;
+// export const PitayaFlex = () => <FlexStory />;
+// export const PitayaBreadcrumb = () => <BreadcrumbStorybook />;
+// export const PitayaButton = () => <ButtonStorybook />;
+// export const PitayaCheckbox = () => <CheckboxStorybook />;
+// export const PitayaTextarea = () => <TextareaStorybook />;
+// export const PitayaSelect = () => <SelectStorybook />;
+// export const _PitayaFail = () => <PitayaFail connectStatus />;
+// export const PitayaFormModal = () => <FormModalStorybook />;
 
-PitayaFormModal.story = {
-  name: 'Pitaya FormModal',
-};
+// PitayaFormModal.story = {
+//   name: 'Pitaya FormModal',
+// };
 
-export const PitayaSwitchButton = () => <SwitchStorybook />;
-export const PitayaTabsButton = () => <TabsStorybook />;
-export const PitayaNestable = () => <NestableStorybook />;
-export const PitayaFileInput = () => <FileInputStorybook />;
+// export const PitayaSwitchButton = () => <SwitchStorybook />;
+// export const PitayaTabsButton = () => <TabsStorybook />;
+// export const PitayaNestable = () => <NestableStorybook />;
+// export const PitayaFileInput = () => <FileInputStorybook />;
 
-PitayaFileInput.story = {
-  name: 'Pitaya FileInput',
-};
+// PitayaFileInput.story = {
+//   name: 'Pitaya FileInput',
+// };
 
-export const PitayaPagination = () => <PaginationStorybook />;
+// export const PitayaPagination = () => <PaginationStorybook />;
 
-PitayaPagination.story = {
-  name: 'pitaya Pagination',
-};
+// PitayaPagination.story = {
+//   name: 'pitaya Pagination',
+// };
 
-export const PitayaCheckOptions = () => <CheckOptionsStorybook />;
+// export const PitayaCheckOptions = () => <CheckOptionsStorybook />;
 
-PitayaCheckOptions.story = {
-  name: 'pitaya CheckOptions',
-};
+// PitayaCheckOptions.story = {
+//   name: 'pitaya CheckOptions',
+// };
 
-export const PitayaConfirm = () => <ConfirmStorybook />;
+// export const PitayaConfirm = () => <ConfirmStorybook />;
 
-PitayaConfirm.story = {
-  name: 'pitaya Confirm',
-};
+// PitayaConfirm.story = {
+//   name: 'pitaya Confirm',
+// };
 
-export const PitayaTooltip = () => <PitayaTooltipStory />;
-export const PitayaActionSelector = () => <PitayaActionSelectorStory />;
+// export const PitayaTooltip = () => <PitayaTooltipStory />;
+// export const PitayaActionSelector = () => <PitayaActionSelectorStory />;
 
-PitayaActionSelector.story = {
-  name: 'Pitaya ActionSelector',
-};
+// PitayaActionSelector.story = {
+//   name: 'Pitaya ActionSelector',
+// };
 
-window.i18n = i18n;
+// window.i18n = i18n;
